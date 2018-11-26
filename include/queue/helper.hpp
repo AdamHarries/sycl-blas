0cd6b2be01ddc4a796dcb901ada5ec22a6957d2c
Adam Harries <adam.harries@codeplay.com>
2018-11-26 13:33:16 +0000

    update helper interfaces with property lists

--- include/queue/helper.hpp
+++ include/queue/helper.hpp
@@ -5,28 +5,37 @@
 
 namespace blas {
 namespace helper {
+
+// construct buffer from host pointer/data
 template <typename scalar_t, typename index_t>
-inline blas::buffer_iterator<scalar_t> make_sycl_iterator_buffer(scalar_t* data,
-                                                                 index_t size) {
+inline blas::buffer_iterator<scalar_t> make_sycl_iterator_buffer(
+    scalar_t* data, index_t size,
+    const cl::sycl::property_list& propList = {}) {
   using buff_t = blas::buffer_t<scalar_t, 1>;
   return blas::buffer_iterator<scalar_t>{
-      buff_t{data, cl::sycl::range<1>(size)}};
+      buff_t{data, cl::sycl::range<1>(size), propList}};
 }
 
+// construct buffer from std::vector
 template <typename scalar_t, typename index_t>
 inline buffer_iterator<scalar_t> make_sycl_iterator_buffer(
-    std::vector<scalar_t>& data, index_t size) {
+    std::vector<scalar_t>& data, index_t size,
+    const cl::sycl::property_list& propList = {}) {
   using buff_t = blas::buffer_t<scalar_t, 1>;
   return blas::buffer_iterator<scalar_t>{
-      buff_t{data.data(), cl::sycl::range<1>(size)}};
+      buff_t{data.data(), cl::sycl::range<1>(size), propList}};
 }
 
+// construct buffer with size, no data
 template <typename scalar_t, typename index_t>
-inline blas::buffer_iterator<scalar_t> make_sycl_iterator_buffer(index_t size) {
+inline blas::buffer_iterator<scalar_t> make_sycl_iterator_buffer(
+    index_t size, const cl::sycl::property_list& propList = {}) {
   using buff_t = blas::buffer_t<scalar_t, 1>;
-  return blas::buffer_iterator<scalar_t>{buff_t{cl::sycl::range<1>(size)}};
+  return blas::buffer_iterator<scalar_t>{
+      buff_t{cl::sycl::range<1>(size), propList}};
 }
 
+// construct buffer from buffer
 template <typename scalar_t, typename index_t>
 inline blas::buffer_iterator<scalar_t> make_sycl_iterator_buffer(
     blas::buffer_t<scalar_t, 1> buff_) {
